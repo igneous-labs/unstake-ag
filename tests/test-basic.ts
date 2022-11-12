a838c8f927d6257a127ca3b74f03b2fb63b5f7a7
billythedummy <dhy1996@live.com.sg>
2022-11-12 15:01:35 +0800

    simulate setup too

--- tests/test-basic.ts
+++ tests/test-basic.ts
@@ -157,20 +157,16 @@ async function checkRoutes(
         "cleanup:",
         cleanupTransaction?.serialize(SERIALIZE_CONFIG_MOCK_SIG).length,
       );
-      // try simulating transactions with no setupTransactions to
+      // try simulating setupTransaction or unstakeTransaction to
       // make sure they work
-      if (!setupTransaction) {
-        const sim = await CONN.simulateTransaction(
-          unstakeTransaction,
-          undefined,
-        );
-        expect(
+      const txToSim = setupTransaction || unstakeTransaction;
+      const sim = await CONN.simulateTransaction(txToSim, undefined);
+      expect(
+        sim.value.err,
+        `Failed to simulate ${routeLabel}\nError: ${JSON.stringify(
           sim.value.err,
-          `Failed to simulate ${routeLabel}\nError: ${JSON.stringify(
-            sim.value.err,
-          )}\nLogs:\n${sim.value.logs?.join("\n")}`,
-        ).to.be.null;
-      }
+        )}\nLogs:\n${sim.value.logs?.join("\n")}`,
+      ).to.be.null;
     }),
   );
   expect(
