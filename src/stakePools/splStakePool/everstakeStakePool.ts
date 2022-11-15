053e5c36ed4861e8dd621a260c42707a892746ad
billythedummy <dhy1996@live.com.sg>
2022-11-15 13:30:25 +0800

    refactor createSwapInstructions

--- src/stakePools/splStakePool/everstakeStakePool.ts
+++ src/stakePools/splStakePool/everstakeStakePool.ts
@@ -1,11 +1,8 @@
 /* eslint-disable max-classes-per-file */
 
-import { TOKEN_PROGRAM_ID } from "@solana/spl-token-v2";
 import {
   LAMPORTS_PER_SOL,
   PublicKey,
-  StakeAuthorizationLayout,
-  StakeProgram,
   Struct,
   TransactionInstruction,
 } from "@solana/web3.js";
@@ -22,7 +19,6 @@ import {
 import {
   AccountType,
   decodeValidatorList,
-  depositStakeInstruction,
   Fee,
   Lockup,
 } from "@soceanfi/stake-pool-sdk";
@@ -35,6 +31,7 @@ import type {
   StakePoolQuoteParams,
 } from "@/unstake-ag/stakePools";
 import { SplStakePool } from "@/unstake-ag/stakePools/splStakePool/splStakePool";
+import { decrementStakePoolIxData } from "@/unstake-ag/stakePools/splStakePool/utils";
 
 /**
  * Layouts and typdefs copied from
@@ -228,61 +225,12 @@ export class EverstakeSplStakePool extends SplStakePool {
     );
   }
 
-  override createSwapInstructions({
-    stakeAccountPubkey,
-    stakerAuth,
-    withdrawerAuth,
-    destinationTokenAccount,
-    stakeAccountVotePubkey,
-    feeAccount: referrer,
-  }: CreateSwapInstructionsParams): TransactionInstruction[] {
-    if (!this.stakePool) {
-      throw new Error("stakePool not fetched");
-    }
-    // TODO: export sync versions of these PDA util functions
-    // from stake-pool-sdk
-    const [stakePoolWithdrawAuth] = PublicKey.findProgramAddressSync(
-      [this.stakePoolAddr.toBuffer(), Buffer.from("withdraw")],
-      this.programId,
-    );
-    const [validatorStakeAccount] = PublicKey.findProgramAddressSync(
-      [stakeAccountVotePubkey.toBuffer(), this.stakePoolAddr.toBuffer()],
-      this.programId,
-    );
-    const depositStakeIx = depositStakeInstruction(
-      this.programId,
-      this.stakePoolAddr,
-      this.validatorListAddr,
-      this.stakePool.depositAuthority,
-      stakePoolWithdrawAuth,
-      stakeAccountPubkey,
-      validatorStakeAccount,
-      this.stakePool.reserveStake,
-      destinationTokenAccount,
-      this.stakePool.managerFeeAccount,
-      referrer ?? this.stakePool.managerFeeAccount,
-      this.stakePool.poolMint,
-      TOKEN_PROGRAM_ID,
-    );
-    // monkey-patch data field to the correct byte for DepositStake
-    // because SPL 0.7.0 merged CreateValidatorStakeAccount and AddValidatorToPool into one instruction,
-    // so any instruction other than Initialize is offset by -1 from Socean's version
-    depositStakeIx.data = Buffer.from([9]);
-    return [
-      ...StakeProgram.authorize({
-        stakePubkey: stakeAccountPubkey,
-        authorizedPubkey: stakerAuth,
-        newAuthorizedPubkey: this.stakePool.depositAuthority,
-        stakeAuthorizationType: StakeAuthorizationLayout.Staker,
-      }).instructions,
-      ...StakeProgram.authorize({
-        stakePubkey: stakeAccountPubkey,
-        authorizedPubkey: withdrawerAuth,
-        newAuthorizedPubkey: this.stakePool.depositAuthority,
-        stakeAuthorizationType: StakeAuthorizationLayout.Withdrawer,
-      }).instructions,
-      depositStakeIx,
-    ];
+  override createSwapInstructions(
+    args: CreateSwapInstructionsParams,
+  ): TransactionInstruction[] {
+    const ixs = super.createSwapInstructions(args);
+    decrementStakePoolIxData(this.programId, ixs);
+    return ixs;
   }
 
   override update(accountInfoMap: AccountInfoMap): void {
